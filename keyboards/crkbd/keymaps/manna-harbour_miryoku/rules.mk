f84f32aaf280723c5e5a93d041856737f86ff3db
Chirag Joshi <chirag85@gmail.com>
2023-09-09 12:38:31 +1000

    Turn off features to bring firmware size down

--- keyboards/crkbd/keymaps/manna-harbour_miryoku/rules.mk
+++ keyboards/crkbd/keymaps/manna-harbour_miryoku/rules.mk
@@ -1,5 +1,7 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
-LTO_ENABLE      = yes
+MOUSEKEY_ENABLE      = yes    # Mouse keys
+RGBLIGHT_ENABLE      = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE          = yes
+OLED_DRIVER          = SSD1306
+LTO_ENABLE           = yes
+RGBLIGHT_SUPPORTED   = no
+RGB_MATRIX_SUPPORTED = no
